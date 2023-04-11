117d73e7ab62b4b13e94e27d015c6781501b17d7
Yogesh Nikam <36443045+yogeshk04@users.noreply.github.com>
2023-04-11 10:43:32 +0200

    Create jenkins-installation-ubuntu-22.sh
    Jenkins installation on ubuntu 22.04

---
+++ jenkins-installation-ubuntu-22.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+supo apt update
+sudo apt update
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+    
+sudo apt-get update
+sudo apt-get install fontconfig openjdk-11-jre
+sudo apt-get install jenkins
+sudo systemctl status jenkins
+java -version
+sudo sysctl start jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
