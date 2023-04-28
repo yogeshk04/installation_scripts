b6699bc735d4f9387fc2266e001759c118a4ccb4
Yogesh Nikam <36443045+yogeshk04@users.noreply.github.com>
2023-04-28 13:35:51 +0200

    Update jenkins-installation-ubuntu-22.sh

--- jenkins-installation-ubuntu-22.sh
+++ jenkins-installation-ubuntu-22.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 supo apt update
-sudo apt update
+sudo apt upgrade
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
     /usr/share/keyrings/jenkins-keyring.asc > /dev/null
     
@@ -10,7 +10,7 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
     
 sudo apt-get update
-sudo apt-get install fontconfig openjdk-11-jre
+sudo apt-get install fontconfig openjdk-17-jre
 sudo apt-get install jenkins
 sudo systemctl status jenkins
 java -version
